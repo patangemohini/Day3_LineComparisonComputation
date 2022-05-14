4e6ccebf50025de14fa9bc8de4e481fd0d9ba44f
mohini patange <patangemohini5@gmail.com>
2022-05-15 00:28:43 +0530

    comparing two lines are equal or not

--- src/linecomparison/LineComparison.java
+++ src/linecomparison/LineComparison.java
@@ -6,7 +6,7 @@ public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program!!");
         int x1, y1, x2, y2;
-        System.out.println("Please Enter the co-ordinates of Line");
+        System.out.println("Please Enter the co-ordinates of 1st Line");
         Scanner scanner = new Scanner(System.in);
         x1 = scanner.nextInt();
         y1 = scanner.nextInt();
@@ -14,11 +14,19 @@ public class LineComparison {
         y2 = scanner.nextInt();
         double length1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
         System.out.println("length = " + length1);
+
+        System.out.println("Please Enter the co-ordinates of 2st Line");
         x1 = scanner.nextInt();
         y1 = scanner.nextInt();
         x2 = scanner.nextInt();
         y2 = scanner.nextInt();
         double length2 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
         System.out.println("length = " + length2);
+        if (length1 == length2) {
+            System.out.println("length of lines are equal");
+        } else {
+            System.out.println("length of lines are not equal");
+        }
     }
 }
+
