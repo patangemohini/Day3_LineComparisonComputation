df0f718b9df65df22e7587c2fc066aab8256b535
mohini patange <patangemohini5@gmail.com>
2022-05-15 00:11:52 +0530

    calculated length

--- src/linecomparison/LineComparison.java
+++ src/linecomparison/LineComparison.java
@@ -1,7 +1,24 @@
 package linecomparison;
 
+import java.util.Scanner;
+
 public class LineComparison {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program!!");
+        int x1, y1, x2, y2;
+        System.out.println("Please Enter the co-ordinates of Line");
+        Scanner scanner = new Scanner(System.in);
+        x1 = scanner.nextInt();
+        y1 = scanner.nextInt();
+        x2 = scanner.nextInt();
+        y2 = scanner.nextInt();
+        double length1 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+        System.out.println("length = " + length1);
+        x1 = scanner.nextInt();
+        y1 = scanner.nextInt();
+        x2 = scanner.nextInt();
+        y2 = scanner.nextInt();
+        double length2 = Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+        System.out.println("length = " + length2);
     }
 }
